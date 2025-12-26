6d617b20d92a4834ca1f9d7c9b2f0648ac04310d
holmen1 <holmen1@gmail.com>
2025-12-27 00:04:48 +0100

    add lsp config

--- src/bf.c
+++ src/bf.c
@@ -3,28 +3,29 @@
  * Portable: Works on Linux, FreeBSD, macOS, and any POSIX system
  */
 
+#include "bf.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "bf.h"
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <filename.bf>\n", argv[0]);
-    return 1;
-  }
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename.bf>\n", argv[0]);
+        return 1;
+    }
 
-  // Read Brainfuck source file
-  char program[PROGRAM_SIZE];
-  int program_length = read_program(argv[1], program, PROGRAM_SIZE);
+    // Read Brainfuck source file
+    char program[PROGRAM_SIZE];
+    int program_length = read_program(argv[1], program, PROGRAM_SIZE);
 
-  if (program_length < 0) {
-    return 1; // Error message already printed by read_program()
-  }
+    if (program_length < 0) {
+        return 1; // Error message already printed by read_program()
+    }
 
-  execute_program(program, program_length);
+    execute_program(program, program_length);
 
-  return (program_length > 0) ? 0 : 1;
+    return (program_length > 0) ? 0 : 1;
 }
 
 /**
@@ -38,28 +39,29 @@ int main(int argc, char *argv[]) {
  * Returns:
  *   Number of bytes read, or -1 on error
  */
-int read_program(const char *filename, char *buffer, int max_size) {
-  FILE *file = fopen(filename, "r");
-  if (!file) {
-    fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
-    return -1;
-  }
+int read_program(const char *filename, char *buffer, int max_size)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        fprintf(stderr, "Error: Cannot open file '%s'\n", filename);
+        return -1;
+    }
 
-  int length = 0;
-  int ch;
-  while ((ch = fgetc(file)) != EOF && length < max_size) {
-    buffer[length++] = ch;
-  }
+    int length = 0;
+    int ch;
+    while ((ch = fgetc(file)) != EOF && length < max_size) {
+        buffer[length++] = ch;
+    }
 
-  fclose(file);
+    fclose(file);
 
-  // Check if file was too large
-  if (ch != EOF && length == max_size) {
-    fprintf(stderr, "Error: Program too large (max %d bytes)\n", max_size);
-    return -1;
-  }
+    // Check if file was too large
+    if (ch != EOF && length == max_size) {
+        fprintf(stderr, "Error: Program too large (max %d bytes)\n", max_size);
+        return -1;
+    }
 
-  return length;
+    return length;
 }
 
 /*
@@ -69,51 +71,53 @@ int read_program(const char *filename, char *buffer, int max_size) {
  *   program        - Brainfuck source code
  *   program_length - Number of instructions
  */
-void execute_program(const char *program, int program_length) {
-  unsigned char memory[MEMORY_SIZE] = {0};
-  unsigned char *cell = memory;  /* pointer to current cell */
+void execute_program(const char *program, int program_length)
+{
+    unsigned char memory[MEMORY_SIZE] = {0};
+    unsigned char *cell = memory; /* pointer to current cell */
 
-  /* Main execution loop */
-  for (int pc = 0; pc < program_length; pc++) {
-    char cmd = program[pc];
+    /* Main execution loop */
+    for (int pc = 0; pc < program_length; pc++) {
+        char cmd = program[pc];
 
-    switch (cmd) {
-    case '>':
-      cell++;
-      break;
-    case '<':
-      cell--;
-      break;
-    case '+':
-      (*cell)++;
-      break;
-    case '-':
-      (*cell)--;
-      break;
-    case '.':
-      putchar(*cell);
-      break;
-    case ',':
-      *cell = getchar();
-      break;
-    case '[':
-      if (*cell == 0) {
-        pc = find_matching_bracket(program, pc, +1);
-      }
-      break;
-    case ']':
-      if (*cell != 0) {
-        pc = find_matching_bracket(program, pc, -1);
-      }
-      break;
-    default:
-      /* ignore other characters */
-      break;
+        switch (cmd) {
+        case '>':
+            cell++;
+            break;
+        case '<':
+            cell--;
+            break;
+        case '+':
+            (*cell)++;
+            break;
+        case '-':
+            (*cell)--;
+            break;
+        case '.':
+            putchar(*cell);
+            break;
+        case ',':
+            *cell = getchar();
+            break;
+        case '[':
+            if (*cell == 0) {
+                pc = find_matching_bracket(program, pc, +1);
+            }
+            break;
+        case ']':
+            if (*cell != 0) {
+                pc = find_matching_bracket(program, pc, -1);
+            }
+            break;
+        default:
+            /* ignore other characters */
+            break;
+        }
     }
-  }
 }
 
-int find_matching_bracket(const char *program, int pos, int direction) {
+int find_matching_bracket(const char *program, int pos, int direction)
+{
     int depth = 1;
     while (1) {
         pos += direction;
